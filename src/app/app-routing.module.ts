e94fa03016e01d396fb03bbf2f633e5df94580ae
= <=>
2022-03-31 22:56:30 -0500

    primeros pasos (instalacion, configuracion, creacion de componente, diseñar)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { IndexComponent } from './product/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'product',
+    component: IndexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './product/index/index.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { FooterComponent } from './widget/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    WelcomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
